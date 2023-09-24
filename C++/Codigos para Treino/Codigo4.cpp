044979253c9debafbf6df296385c8f23ee05fe60
Gabriel Alves Pereira <145914573+Gab-Zag@users.noreply.github.com>
2023-09-24 14:47:19 -0300

    Create Codigo4.cpp

---
+++ C++/Codigos para Treino/Codigo4.cpp
@@ -0,0 +1,24 @@
+#include "iostream"
+
+using namespace std;
+
+struct dados
+{
+    int k;
+    int status;
+};
+
+int main()
+{
+    dados t [10];
+    int m;
+    int i;
+
+    cin >> m;
+
+    for(i = 0; i < m; i++)
+    {
+        tabela[i].status = 0;
+    }
+    return 0;
+}
